f28d6731e3a804524749a8ae7af7b323298bc96b
Mikhail19870105 <crazyhtc87@gmail.com>
2020-05-04 18:42:07 +0300

    Modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        float TicketPriceRub = 8595.15F;
-        int PriceInKops = (int) (TicketPriceRub * 100); //Итоговая стоимость билета в копейках
-        float OneMileCostRub =  20F;
-        int OneMileCostKops = (int) (OneMileCostRub * 100); // Стоимость одной бонусной мили в копейках
-        int MilesAmount =  PriceInKops / OneMileCostKops;
-        System.out.println(MilesAmount);
+        float ticketPriceRub = 8595.15F;
+        int priceInKops = (int) (ticketPriceRub * 100); //Итоговая стоимость билета в копейках
+        float oneMileCostRub =  20F;
+        int oneMileCostKops = (int) (oneMileCostRub * 100); // Стоимость одной бонусной мили в копейках
+        int milesAmount =  priceInKops / oneMileCostKops;
+        System.out.println(milesAmount);
     }
 }
